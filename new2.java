d6ecf3b1bd5f4c06defd2be2f272d848725f1be4
Tushar MV <49874367+1o26@users.noreply.github.com>
2025-12-05 09:41:34 +0530

    Create new2.java with main method

--- new2.java
+++ new2.java
@@ -1,2 +1,6 @@
-System.out.println("Hello Java");
-System.out.println("Goodbye Java");
+public class new2{
+  public static void main(String[] args){
+    System.out.println("Hello Java");
+    System.out.println("Goodbye Java")
+  }
+}
