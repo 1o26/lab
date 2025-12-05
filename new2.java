8bb1e2408c34501daed7ad9b054ee6ce9234d11a
Tushar MV <49874367+1o26@users.noreply.github.com>
2025-12-05 09:42:24 +0530

    Fix missing semicolon in Goodbye Java print statement

--- new2.java
+++ new2.java
@@ -1,6 +1,6 @@
 public class new2{
   public static void main(String[] args){
     System.out.println("Hello Java");
-    System.out.println("Goodbye Java")
+    System.out.println("Goodbye Java");
   }
 }
